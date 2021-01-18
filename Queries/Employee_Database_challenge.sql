76bf202f423b8509f92b638f16240eecdbf9192f
Jackson Sheppard <sheppard@slac.stanford.edu>
2021-01-17 20:35:17 -0800

    ENH: table of mentorship program eligible employees, deliverable 2 complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,3 +29,22 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
+
+-- Deliverable 2: Employees Eligible for the Mentorship Program
+-- Table of employees born 1965-01-01 to 1965-12-31
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no, ti.to_date DESC;
